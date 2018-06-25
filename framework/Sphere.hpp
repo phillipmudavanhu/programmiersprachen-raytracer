c7d80d9c8423dd3edf788d6080c14c474354974a
phillipmudavanhu <philcreativ@gmail.com>
2018-06-25 04:16:14 +0200

    5.2

---
+++ framework/Sphere.hpp
@@ -0,0 +1,34 @@
+#ifndef RAYTRACER_SPHERE_HPP
+#define RAYTRACER_SPHERE_HPP
+
+
+#include "color.hpp"
+#include <iostream>
+#include <string>
+
+ //Base Class to be Inheritated
+class Shape 
+{
+
+public:
+	// Constructors
+	Shape();
+   	Shape(std::string const& name, Color const& color);
+   	~Shape();
+
+	// Getter
+  std::string getName() const;
+  Color getColor() const;
+
+   //Virtual pure methods
+   virtual void area() const = 0;
+   virtual void volume() const = 0;
+   virtual std::ostream& print(std::ostream& os) const;
+
+private:
+   std::string name_;
+   Color color_; 
+};
+
+
+#endif //RAYTRACER_SPHERE_HPP
